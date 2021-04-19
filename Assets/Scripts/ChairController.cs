a085c8d08b6a39c646f68ec374261e5b515c3914
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-04-19 14:06:22 -0400

    Initial Commit

---
+++ Assets/Scripts/ChairController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairController : MonoBehaviour
+{
+    public float riseSpeed = 1.0f;
+    public float shakeSpeed = 1.0f;
+    public float shakeSize = 1.0f;
+    public float maxHeight;
+    public float delay;
+    public Light overheadLight;
+    public Light flashlight;
+    public Color scareColor;
+    public GameObject toy;
+    private float minHeight = -0.34f;
+    private float riseTime = 1.0f;
+    private bool sceneComplete = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("shakeChair", shakeSpeed, delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < maxHeight && !sceneComplete){
+            transform.Translate(Vector3.up * Time.deltaTime * riseSpeed);
+            shakeChair();
+        }else{
+            endScare();
+        }
+
+    }
+
+    void shakeChair(){
+        gameObject.transform.Rotate(0, shakeSize * Time.deltaTime, 0);
+
+    }
+
+    void endScare(){
+        overheadLight.enabled = false;
+        flashlight.enabled = false;
+        flashlight.color = scareColor;
+        toy.SetActive(true);
+        gameObject.SetActive(false);
+        flashlight.enabled = true;
+        sceneComplete = true;
+    }
+
+}
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float flickerSpeed = 0.1f;
+    public float flickersPerSecond = 3.0f;
+    public float intensity = 0.2f;
+
+    private float time;
+    private float start_intensity;
+    private Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        start_intensity = light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * (1- Random.Range(-flickerSpeed, flickerSpeed)) * Mathf.PI;
+        light.intensity = start_intensity + Mathf.Sin(time * flickersPerSecond) * intensity;
+    }
+}
